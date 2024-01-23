773ac581ca1f5608ba0e43e86dbe7e9ab5c19af7
Damir747 <damir_shakirov@list.ru>
2024-01-23 10:15:06 +0300

    ways from X and I, but only one I

--- js-f.js
+++ js-f.js
@@ -4,7 +4,7 @@ const n = 5;
 const m = 5;
 // схема
 const arr = [
-	['.', 'I', '.', '.', '.'],
+	['I', '.', '.', '.', '.'],
 	['.', 'B', '.', 'B', '.'],
 	['B', 'B', '.', 'B', '.'],
 	['.', '.', '.', 'B', '.'],
@@ -30,89 +30,78 @@ console.log('Координаты X:', iX, jX);
 console.log('Координаты I:', arrI);
 
 // массив посещенных точек c результатами длин
-const visited = Array.from(Array(arrI.length), () => Array.from(Array(n), () => Array.from(Array(m), () => false)));
+const visited = Array.from(Array(n), () => Array.from(Array(m), () => false));
 // начальную точку 'X' кладем в очередь
 const queueX = [[[iX, jX]]];
 // конечные точки 'I' кладем в очередь
-const queueI = Array.from(Array(arrI.length), () => []);
+const queueI = [];
+const temp = [];
 for (let k = 0; k < arrI.length; k++) {
-	const temp = [];
 	temp.push([arrI[k].i, arrI[k].j]);
-	queueI[k].push(temp);
 }
-for (let k = 0; k < queueI.length; k++) {
-	console.log(`queueI[${k}]:`, queueI[k]);
-}
+queueI.push(temp);
 // начальное положение указателя в очереди
 let countX = 0;
-let countI = Array.from(Array(arrI.length), () => 0);
+let countI = 0;
 
 // поиск друзей точки
-const friends = (queue, len, checkX) => {
+const friends = (queue, len) => {
 	queue.push([]);
 	const temp = [];
-	if (checkX) {
-
-	} else {
-		queue[len].forEach(el => {
-			const [x, y] = el;
-			if (!visited[0][x][y]) {
-				console.log('Смотрю точку:', x, y);
-				visited[0][x][y] = true;
-				// если точка не на краю, не посещенная, не занята 'B',
-				// тогда добавить в очередь
-				if (x > 0 && !visited[0][x - 1][y] && arr[x - 1][y] !== 'B') {
-					temp.push([x - 1, y]);
-				}
-				if (x < arr.length - 1 && !visited[0][x + 1][y] && arr[x + 1][y] !== 'B') {
-					temp.push([x + 1, y]);
-				}
-				if (y > 0 && !visited[0][x][y - 1] && arr[x][y - 1] !== 'B') {
-					temp.push([x, y - 1]);
-				}
-				if (y < arr[0].length - 1 && !visited[0][x][y + 1] && arr[x][y + 1] !== 'B') {
-					temp.push([x, y + 1]);
-				}
+	queue[len].forEach(el => {
+		const [x, y] = el;
+		if (!visited[x][y]) {
+			// console.log('Смотрю точку:', x, y);
+			visited[x][y] = true;
+			// если точка не на краю, не посещенная, не занята 'B',
+			// тогда добавить в очередь
+			if (x > 0 && !visited[x - 1][y] && arr[x - 1][y] !== 'B') {
+				temp.push([x - 1, y]);
 			}
-		});
-		queue[len + 1] = temp;
-	}
+			if (x < arr.length - 1 && !visited[x + 1][y] && arr[x + 1][y] !== 'B') {
+				temp.push([x + 1, y]);
+			}
+			if (y > 0 && !visited[x][y - 1] && arr[x][y - 1] !== 'B') {
+				temp.push([x, y - 1]);
+			}
+			if (y < arr[0].length - 1 && !visited[x][y + 1] && arr[x][y + 1] !== 'B') {
+				temp.push([x, y + 1]);
+			}
+		}
+	});
+	queue[len + 1] = temp;
 }
 
-const check = (k) => {
+const check = () => {
 	// если очередь X не пуста и если очередь I не пуста
-	while (queueX[countX].length > 0 && queueI[k][countI[k]].length > 0) {
-		console.log(`внутри цикла check(${k})`);
+	while (queueX[countX].length > 0 && queueI[countI].length > 0) {
 		let x = 0;
 		while (x < queueX[countX].length) {
 			let i = 0;
-			while (i < queueI[k][countI[k]].length) {
-				if (queueX[countX][x][0] === queueI[k][countI[k]][i][0] && queueX[countX][x][1] === queueI[k][countI[k]][i][1]) {
-					console.log('Ответ:', countX + countI[k]);
-					return countX + countI[k];
+			while (i < queueI[countI].length) {
+				if (queueX[countX][x][0] === queueI[countI][i][0] && queueX[countX][x][1] === queueI[countI][i][1]) {
+					console.log('Ответ:', countX + countI);
+					return countX + countI;
 				}
 				i++;
 			}
 			x++;
 		}
 
-		if (countX === countI[k]) {
-			friends(queueX, countX, true);
+		if (countX === countI) {
+			friends(queueX, countX);
 			countX++;
 		} else {
-			friends(queueI, countI[k], false);
-			countI[k]++;
+			friends(queueI, countI);
+			countI++;
 		}
 	}
 	console.log('Ответ:', -1);
 	return -1;
 }
 
-for (let k = 0; k < arrI.length; k++) {
-	check(k);
-}
-
-// console.log(visited[0]);
+check();
+// console.log(visited);
 // if (count > 0) {
 // 	console.log(-1, 'решения нет');
 // }
@@ -121,7 +110,7 @@ const result = [];
 // найти максимальную длину
 let max = 0;
 for (let k = 0; k < arrI.length; k++) {
-	const res = visited[0][arrI[k].i][arrI[k].j];
+	const res = visited[arrI[k].i][arrI[k].j];
 	result.push(res);
 	if (max < res) {
 		max = res;
